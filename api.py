9a7be34a7981a079ad07f566543c984058e1a333
mrpojam <jafari.pouya79@outlook.com>
2025-03-14 13:27:23 +0100

    debugged the api service

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from unsloth import FastLanguageModel
 
 app = FastAPI()
 
-repo_name = "mrpojam/Llama-De-Fr"  
+repo_name = "mrpojam/Llama3.2-1B-De2Fr-Translation"  
 
 tokenizer = AutoTokenizer.from_pretrained(repo_name)
 model = FastLanguageModel.from_pretrained(repo_name, device_map="cuda")[0]
@@ -17,6 +17,22 @@ class TranslationRequest(BaseModel):
 
 @app.post("/translate")
 async def translate(request: TranslationRequest):
-    result = model.generate(**request.text, max_new_tokens = 32, use_cache = True)
-    result = result.split("Output:")[1].split("<|end_of_text|>")[0].replace("\n", "")
-    return {"translation": result}
+    data_prompt = """Translate the following German text to French:
+
+    ### Input:
+    {}
+
+    ### Output:
+    {}"""
+
+    prompt = tokenizer(
+    [
+        data_prompt.format(
+            request,
+            "",
+        )
+    ], return_tensors = "pt").to("cuda")
+    result = model.generate(**prompt, max_new_tokens = 128, use_cache = True)
+    decoded = tokenizer.batch_decode(result)
+    translation = decoded.split("Output:")[1].split("<|end_of_text|>")[0].replace("\n", "")
+    return {"translation": translation}
